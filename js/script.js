71d80d990a378840daba674fc26449d03ad0cc4d
Nikita Rai <nikrc15@gmail.com>
2017-04-17 23:43:01 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -36,9 +36,35 @@ function loadData() {
         var article = articles[i];
         $nytElem.append('<li class="article">' + '<a href="' + article.web_url + '">' + article.headline.main + '</a>' + '<p>' + article.snippet + '</p>' + '</li>');
       };
+    }).error(function(e){
+      $nytHeaderElem.text('New York Times Articles Could Not Be Loaded.');
     });
+
+    var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + cityStr + '&format=json&callback=wikiCallback';
+
+    var wikiRequestTimeout = setTimeout(function() {
+      $wikiElem.text("Failed to get Wikipedia Resources.");
+    }, 8000);
+
+    $.ajax({
+      url: wikiUrl,
+      dataType: "jsonp",
+      //jsonp: "callback",
+      success: function( response ) {
+        var articleList = response[1];
+
+        for(var i = 0; i < articleList.length; i++) {
+          articleStr = articleList[i];
+          var url = 'http://en.wikipedia.org/wiki/' + articleStr;
+          $wikiElem.append('<li><a href="' + url + '">' + articleStr + '</a></li>');
+        };
+        clearTimeout(wikiRequestTimeout);
+      }
+    }) ;
 
     return false;
 };
+
+
 
 $('#form-container').submit(loadData);
